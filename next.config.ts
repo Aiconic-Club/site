dcd606ecc08827623551b930525568f56f4d0891
RasulGhatane <rasulghatane@gmail.com>
2025-04-27 15:56:03 +0545

    images fixed once again

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    unoptimized: true
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true
